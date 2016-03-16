83e12b526725ac17a7456d6e28bd5473247cee0b
Yeap Hooi Tong <hooitongx@gmail.com>
2016-03-16 20:40:21 +0800

    Converted project to full MEAN stack and setup REST framework

---
+++ public/GruntFile.js
@@ -0,0 +1,105 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        bower: grunt.file.readJSON('bower.json'),
+        clean: {
+            dist: ['dist/*'],
+            tmp : ['.tmp'],
+            js : ['dist/scripts']
+        },
+        copy: {
+            dist: {
+                files: [
+                    {expand: true, cwd: 'app/', src: '**', dest: 'dist/app/'},
+                    {expand: true, cwd: 'assets/', src: ['**', '!**/scss/**'], dest: 'dist/assets/'},
+                    {expand: true, cwd: 'libs/', src: '**', dest: 'dist/libs/'}
+                ]
+            },
+            js: {
+                files: [
+                    {src: 'dist/scripts/app.angular.js', dest : 'dist/app/scripts/app.angular.js'}
+                ]
+            },
+            libs:{
+                files: '<%= bower.copy %>'
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: { removeComments: true, collapseWhitespace: true },
+                files: [
+                    { expand: true, cwd: 'views/', src: ['*.html', '**/*.html'], dest: 'dist/views/' }
+                ]
+            }
+        },
+        watch: {
+            sass: {
+              files: ['assets/scss/*.scss'],
+              tasks: ['sass'],
+            }
+        },
+        sass: {
+            dist: {
+                files: [
+                    {'assets/styles/app.css': ['assets/scss/app.scss']}
+                ]
+            }
+        },
+        useminPrepare: {
+            html: ['app/index.html']
+        },
+        usemin: {
+            html: ['dist/app/index.html']
+        },
+        bump: {
+            options: {
+                files: ['package.json'],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['-a'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-assemble');
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'copy',
+        'useminPrepare',
+        'concat:generated',
+        'cssmin:generated',
+        'uglify:generated',
+        'usemin',
+        'sass',
+        'htmlmin',
+        'clean:tmp',
+        'copy:js',
+        'clean:js'
+    ]);
+
+    grunt.registerTask('release', [
+        'bump'
+    ]);
+
+    grunt.registerTask('html', [
+        'clean:html',
+        'assemble'
+    ]);
+};
---
+++ server.js
@@ -0,0 +1,45 @@
+// Packages & Dependencies
+// ====================================================
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const config = require('./config')
+
+// App Configurations
+// ====================================================
+// body parser to grab information from HTTP POST requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+
+// configure app to handle CORS requests
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \Authorization')
+    next()
+})
+
+// log all API requests to console
+app.use(morgan('dev'));
+
+// set static file location for front-end
+app.use(express.static(__dirname + '/public/dist'))
+
+
+// API Routes
+// =====================================================
+var apiRouter = express.Router()
+app.use('/api', apiRouter)
+
+// Catch-All Routing - Sends user to front-end
+// =====================================================
+app.get('*', function(req, res) {
+	res.redirect('/app')
+})
+
+app.listen(config.port)
+console.log('Server Port opened at ' + config.port)
