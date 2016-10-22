82f20742a6021312d7d82da472432401b3d2f17a
Yeap Hooi Tong <hooitongx@gmail.com>
2016-10-22 20:21:46 +0800

    Refreshed users controller for users resource

--- app/routes/users/users.js
+++ app/routes/users/users.js
@@ -24,10 +24,16 @@ function getUser(req, res, next) {
 }
 
 function getUsers(req, res, next) {
+  req.query.range = req.query.range || constants.defaults.range;
+  req.checkQuery('range', `range ${constants.templates.error.invalidData}`).isInt();
+  const errors = req.validationErrors();
+  if (errors) return next(boom.badRequest(errors));
+
   const dateRange = req.query.range;
   const convertedRange = moment(new Date())
     .subtract(dateRange, 'day')
     .format('YYYY-MM-DD HH:mm:ss');
+
   const response = (users) => {
     if (_.isNil(users)) return next(boom.badRequest(constants.templates.error.badRequest));
     res.status(200).json(users);
@@ -38,6 +44,29 @@ function getUsers(req, res, next) {
     .catch(next);
 }
 
-const usersAPI = { getUser, getUsers };
+function getUserProjects(req, res, next) {
+  req.checkParams('userId', `userId ${constants.templates.error.missingParam}`).notEmpty();
+  const errors = req.validationErrors();
+  if (errors) return next(boom.badRequest(errors));
+
+  const userId = req.params.userId;
+
+  const retrieveProjects = (user) => {
+    if (_.isNil(user)) return next(boom.badRequest(constants.templates.error.badRequest));
+    return user.getProjects();
+  };
+
+  const response = (projects) => {
+    if (_.isNil(projects)) return next(boom.badRequest(constants.templates.error.badRequest));
+    res.status(200).json(projects);
+  };
+
+  return models.app.user.getUserById(userId)
+    .then(retrieveProjects)
+    .then(response)
+    .catch(next);
+}
+
+const usersAPI = { getUser, getUsers, getUserProjects };
 
 export default usersAPI;
