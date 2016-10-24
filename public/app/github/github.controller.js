64bdc747a47e17636435bd62db5ab08cdcadcadc
Yeap Hooi Tong <hooitongx@gmail.com>
2016-10-24 22:19:52 +0800

    Changed github controller to provide modals instead

--- public/app/github/github.controller.js
+++ public/app/github/github.controller.js
@@ -16,78 +16,115 @@
     const parent = $scope.$parent;
 
     vm.requestData = () => {
-      const start = parent.dateRange.selected.start;
-      const end = parent.dateRange.selected.end;
-      const range = moment(end).diff(moment(start), 'days');
-      vm.start = start;
-      vm.end = end;
+      vm.range = {
+        start: parent.dateRange.selected.start,
+        end: parent.dateRange.selected.end,
+        days: moment(parent.dateRange.selected.end).diff(moment(parent.dateRange.selected.start), 'days')
+      };
+
       const processResponse = (response) => {
         // replaceable by spread operator with proper promise support
-        vm.repos = response[0].data;
-        vm.commits = response[1].data;
-        vm.releases = response[2].data;
-        vm.participating = response[3].data;
-        vm.users = response[4].data;
-        vm.projects = response[5].data;
+        const repos = response[0].data;
+        const commits = response[1].data;
+        const releases = response[2].data;
+        const participating = response[3].data;
+        const users = response[4].data;
+        const projects = response[5].data;
 
-        // create project map for quick access
-        vm.projectsMap = _.keyBy(vm.projects, 'id');
+        // build projects modal for view usages
+        vm.projects = {
+          data: projects,
+          map: _.keyBy(projects, 'id') // create project map for quick access
+        };
 
+        // build commits modal for view usages
         // process commits to retrieve length, deviation, commits overtime
-        vm.commitsCount = vm.commits.length;
+        const commitsCount = commits.length;
 
         // map each date grouping to number of commits on that day
-        vm.commitsByDate = _
-          .chain(vm.commits)
+        const commitsByDate = _
+          .chain(commits)
           .groupBy(commit => moment(commit.date).startOf('day').valueOf())
           .mapValues(commit => commit.length)
           .toPairs()
           .value();
 
         // calculate mean and standard deviation
-        vm.meanCommits = vm.commitsCount / range;
-        vm.maxPair = _.maxBy(vm.commitsByDate, (dateCount) => {
+        const meanCommits = commitsCount / vm.range.days;
+        const maxPair = _.maxBy(commitsByDate, (dateCount) => {
           return dateCount[1];
         });
-        vm.minPair = _.minBy(vm.commitsByDate, (dateCount) => {
+        const minPair = _.minBy(commitsByDate, (dateCount) => {
           return dateCount[1];
         });
-        vm.deviationCommits = _.reduce(vm.commitsByDate, (sum, pair) => {
-          return sum + Math.pow(pair[1] - vm.meanCommits, 2);
-        }, 0) / range;
+        const deviationCommits = _.reduce(commitsByDate, (sum, pair) => {
+          return sum + Math.pow(pair[1] - meanCommits, 2);
+        }, 0) / vm.range.days;
 
+        vm.commits = {
+          data: commits,
+          count: commitsCount,
+          grouped: commitsByDate,
+          mean: meanCommits,
+          max: maxPair,
+          min: minPair,
+          deviation: deviationCommits
+        };
+
+        // build participation and users modal for viuew usages
         // calculate utilization rate using total users count and unique commits
-        vm.participatingCount = vm.participating.length;
-        vm.usersCount = vm.users.length;
-        vm.utilizationRate = _.round(vm.participatingCount / vm.usersCount, 1);
+        const participatingCount = participating.length;
+        const usersCount = users.length;
+        const utilizationRate = _.round(participatingCount / usersCount, 1);
+        vm.participation = {
+          data: participating,
+          count: participatingCount,
+          utilization: utilizationRate
+        };
 
+        vm.users = {
+          data: users,
+          count: usersCount,
+        };
+
+        // build repos modal for view usages
         // newly created repositories in given date range
-        vm.reposCount = vm.repos.length;
+        vm.repos = {
+          data: repos,
+          count: repos.length
+        };
 
+        // build releases modal for view usages
         // process releases with the number of release with tags, releases count
-        vm.releases = _.map(vm.releases, (release) => {
+        const formattedReleases = _.map(releases, (release) => {
           release.date = moment(release.date).format('DD/MM/YYYY HH:mm:ss');
           return release;
         });
-        vm.releasesCount = vm.releases.length;
+        const releasesCount = formattedReleases.length;
 
         // get number of releases tagged according to tracked tags
         // TODO: Allow admins to add in tags to track
         const sampleTags = ['MS1', 'MS2', 'MS3', '1'];
-        vm.trackedReleases = _.filter(vm.releases, (release) => {
+        const trackedReleases = _.filter(formattedReleases, (release) => {
           return _.includes(sampleTags, release.tagName);
         });
-        vm.trackedReleasesCount = vm.trackedReleases.length;
+        const trackedReleasesCount = trackedReleases.length;
+        vm.releases = {
+          data: formattedReleases,
+          count: releasesCount,
+          tracked: trackedReleases,
+          trackedCount: trackedReleasesCount
+        };
       };
 
       $q
         .all([
-          Github.getRepositories(start, end),
-          Github.getCommits(start, end),
-          Github.getReleases(start, end),
-          Github.getParticipatingUsers(start, end),
-          Users.getUsers(0, end),
-          Projects.getProjects(0, end)
+          Github.getRepositories(vm.range.start, vm.range.end),
+          Github.getCommits(vm.range.start, vm.range.end),
+          Github.getReleases(vm.range.start, vm.range.end),
+          Github.getParticipatingUsers(vm.range.start, vm.range.end),
+          Users.getUsers(0, vm.range.end),
+          Projects.getProjects(0, vm.range.end)
         ])
         .then(processResponse, $log.error);
     };
@@ -96,16 +133,5 @@
       vm.subtitle = 'Collab Statistics on GitHub Usage';
       vm.requestData();
     })();
-
-    // TODO: To be replaced with dynamic data
-    vm.p_l_3 = [
-      [1, 2],
-      [2, 1.6],
-      [3, 2.4],
-      [4, 2.1],
-      [5, 1.7],
-      [6, 1.5],
-      [7, 1.7]
-    ];
   }
 })();
