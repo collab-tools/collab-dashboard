84d739b2003eeb4545f92e1ad65a4c5616719ead
Yeap Hooi Tong <hooitongx@gmail.com>
2016-10-25 16:49:58 +0800

    Refreshed dashboard controller and integrated with view

--- public/app/dashboard/dashboard.controller.js
+++ public/app/dashboard/dashboard.controller.js
@@ -10,65 +10,133 @@
     .controller('dashboardCtrl', dashboardCtrl);
 
   dashboardCtrl.$inject = [
-    '$scope', 'moment', 'Milestones', 'Tasks', 'Drive', 'Github', 'Projects', 'Users',
+    '$scope', '$log', '$q', '_', 'moment', 'Milestones', 'Tasks', 'Drive', 'Github', 'Projects', 'Users'
   ];
 
-  function dashboardCtrl($scope, moment, Milestones, Tasks, Drive, Github, Projects, Users) {
+  function dashboardCtrl($scope, $log, $q, _, moment,
+    Milestones, Tasks, Drive, Github, Projects, Users) {
     const vm = this;
     const parent = $scope.$parent;
     vm.requestData = () => {
-      const start = parent.dateRange.selected.start;
-      const end = parent.dateRange.selected.end;
+      vm.range = {
+        start: parent.dateRange.selected.start,
+        end: parent.dateRange.selected.end,
+        days: moment(parent.dateRange.selected.end).diff(moment(parent.dateRange.selected.start), 'days')
+      };
 
-      // Retrieve number of milestones added
-      Milestones
-        .getMilestones(start, end)
-        .then((response) => { vm.msCount = response.data.length; });
+      const processResponse = (response) => {
+        const milestones = response[0].data;
+        const tasks = response[1].data;
+        const commits = response[2].data;
+        const releases = response[3].data;
+        const files = response[4].data;
+        const changes = response[5].data;
+        const projects = response[6].data;
+        const newUsers = response[7].data;
+        const users = response[8].data;
+        const milestonesUsersPartial = response[9].data;
+        const tasksUsersPartial = response[10].data;
+        const githubUsersPartial = response[11].data;
+        const driveUsersPartial = response[12].data;
 
-      // Retrieve number of tasks added
-      Tasks
-        .getTasks(start, end)
-        .then((response) => { vm.taskCount = response.data.length; });
+        // build individual modals for view usages
+        vm.milestones = {
+          data: milestones,
+          count: milestones.length
+        };
 
-      // Retrieve number of github commits
-      Github
-        .getCommits(start, end)
-        .then((response) => { vm.commitCount = response.data.length; });
+        vm.tasks = {
+          data: tasks,
+          count: tasks.length
+        };
 
-      // Retrieve number of github releases
-      Github
-        .getReleases(start, end)
-        .then((response) => { vm.releaseCount = response.data.length; });
+        vm.commits = {
+          data: commits,
+          count: commits.length
+        };
 
-      // Retrieve number of new files created
-      Drive
-        .getFiles(start, end)
-        .then((response) => { vm.fileCount = response.data.length; });
+        vm.releases = {
+          data: releases,
+          count: releases.length
+        };
 
-      // Retrieve number of file changes
-      Drive
-        .getChanges(start, end)
-        .then((response) => { vm.changeCount = response.data.length; });
+        vm.files = {
+          data: files,
+          count: files.length
+        };
 
-      // Retrieve number of new projects
-      Projects
-        .getProjects(start, end)
-        .then((response) => { vm.projectCount = response.data.length; });
+        vm.changes = {
+          data: changes,
+          count: changes.length
+        };
 
-      // Retrieve number of users that signed up during date range
-      Users
-        .getUsers(start, end)
-        .then((response) => { vm.userCount = response.data.length; });
+        vm.projects = {
+          data: projects,
+          count: projects.length
+        };
 
-      // Retrieve number of total registered accounts
-      Users
-        .getUsers(0, moment().valueOf())
-        .then((response) => { vm.totalCount = response.data.length; });
+        vm.newUsers = {
+          data: newUsers,
+          count: newUsers.length
+        };
 
-      // Retrieve unique users and retention rate (Google Analaytics)
-      // Retrieve feature popularity (Google Analytics)
-      // Retrieve site uptime
-      // Retrieve average API load
+        // calculate active users, inactive users and retention rate
+        const usersCount = users.length;
+        const milestonesUsers = _.intersectionWith(users, milestonesUsersPartial,
+          (left, right) => left.id === right.userId);
+        const milestonesUsersCount = milestonesUsers.length;
+        const tasksUsers = _.intersectionWith(users, tasksUsersPartial,
+          (left, right) => left.id === right.userId);
+        const tasksUsersCount = tasksUsers.length;
+        const githubUsers = _.intersectionBy(users, githubUsersPartial, 'githubLogin');
+        const githubUsersCount = githubUsers.length;
+        const driveUsers = _.intersectionBy(users, driveUsersPartial, 'email');
+        const driveUsersCount = driveUsers.length;
+        const activeUsers = _.unionBy(milestonesUsers, tasksUsers, githubUsers, driveUsers, 'id');
+        const activeUsersCount = activeUsers.length;
+        const inactiveUsersCount = usersCount - activeUsersCount;
+        const retentionRate = activeUsersCount / usersCount;
+        const usersDistribution = [
+          [githubUsersCount, 0],
+          [driveUsersCount, 1],
+          [tasksUsersCount, 2],
+          [milestonesUsersCount, 3]
+        ];
+
+        vm.users = {
+          data: users,
+          count: usersCount,
+          activeCount: activeUsersCount,
+          inactiveCount: inactiveUsersCount,
+          milestones: milestonesUsers,
+          milestonesCount: milestonesUsersCount,
+          tasks: tasksUsers,
+          tasksCount: tasksUsersCount,
+          github: githubUsers,
+          githubCount: githubUsersCount,
+          drive: driveUsers,
+          driveCount: driveUsersCount,
+          distribution: usersDistribution,
+          retention: retentionRate
+        };
+      };
+
+      $q
+        .all([
+          Milestones.getMilestones(false, vm.range.start, vm.range.end),
+          Tasks.getTasks(vm.range.start, vm.range.end),
+          Github.getCommits(vm.range.start, vm.range.end),
+          Github.getReleases(vm.range.start, vm.range.end),
+          Drive.getFiles(vm.range.start, vm.range.end),
+          Drive.getChanges(vm.range.start, vm.range.end),
+          Projects.getProjects(vm.range.start, vm.range.end),
+          Users.getUsers(vm.range.start, vm.range.end),
+          Users.getUsers(0, vm.range.end),
+          Milestones.getParticipatingUsers(vm.range.start, vm.range.end),
+          Tasks.getParticipatingUsers(vm.range.start, vm.range.end),
+          Github.getParticipatingUsers(vm.range.start, vm.range.end),
+          Drive.getParticipatingUsers(vm.range.start, vm.range.end)
+        ]).then(processResponse, $log.error);
     };
 
     // Initialize controller by setting subtitle and data
@@ -77,7 +145,7 @@
       vm.requestData();
     })();
 
-
+    // Sample Data to be Removed
     vm.p_p_1 = [{ data: 70, label: 'Free' }, { data: 30, label: 'Busy' }];
     vm.p_b_1 = [
       [1, 0.7],
@@ -93,8 +161,6 @@
       [200, 1],
       [50, 2],
       [76, 3],
-      [10, 4]
     ];
-
   }
 })();
