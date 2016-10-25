eb9d59ae32a4c462cb0a770fd9699cc5726443c2
Yeap Hooi Tong <hooitongx@gmail.com>
2016-10-25 16:51:01 +0800

    Refreshed drive controller and integrated with view

--- public/app/drive/drive.controller.js
+++ public/app/drive/drive.controller.js
@@ -4,70 +4,135 @@
  * @namespace DriveCtrl
  */
 
-/* global moment */
 (() => {
   angular
     .module('app')
     .controller('driveCtrl', driveCtrl);
 
-  driveCtrl.$inject = ['$scope', '$log', '_', 'Drive'];
+  driveCtrl.$inject = [
+    '$scope', '$log', '$q', '_', 'moment', 'googleMIME', 'Drive', 'Projects', 'Users'
+  ];
 
-  function driveCtrl($scope, $log, _, Drive) {
+  function driveCtrl($scope, $log, $q, _, moment, googleMIME, Drive, Projects, Users) {
     const vm = this;
     const parent = $scope.$parent;
 
-    vm.subtitle = 'Collab Statistics on Google Drive Usage';
+    vm.requestData = () => {
+      vm.range = {
+        start: parent.dateRange.selected.start,
+        end: parent.dateRange.selected.end,
+        days: moment(parent.dateRange.selected.end).diff(moment(parent.dateRange.selected.start), 'days')
+      };
 
-    const processResponse = (response) => {
-      /* process revisions which includes count, revisions over time, file type shared,
-      number of active teams, deviations */
-      vm.revisions = response.data.revisions;
-      vm.teamsCount = response.data.projectsCount;
-      vm.activeTeams = _.uniqBy(vm.revisions, 'projectId');
-      vm.activeTeamsCount = vm.activeTeams.length;
-      vm.inactiveTeamsCount = vm.teamsCount - vm.activeTeamsCount;
+      const processResponse = (response) => {
+        const files = response[0].data;
+        const changes = response[1].data;
+        const participating = response[2].data;
+        const users = response[3].data;
+        const projects = response[4].data;
 
-      vm.revisionsCount = vm.revisions.length;
-      vm.activeUsersCount = response.data.activeUsersCount;
-      vm.usersCount = response.data.userCount;
-      vm.utilizationRate = vm.activeUsersCount / vm.usersCount;
+        // calculate the number of files and file type distribution
+        const filesCount = files.length;
+        const filesDistribution = _
+          .chain(files)
+          .map((file) => {
+            if (!file.fileExtension) file.fileExtension = googleMIME[file.fileMIME];
+            return file;
+          })
+          .groupBy('fileExtension')
+          .mapValues(files => files.length)
+          .toPairs()
+          .map((pair) => {
+            return { label: pair[0], data: pair[1] };
+          })
+          .value();
 
-      vm.revisionsByDate = _.groupBy(vm.revisions, (revision) => {
-        return moment(revision.date).startOf('day');
-      });
-      vm.revisionsByDate = _.mapValues(vm.revisionsByDate, (revisions) => {
-        return revisions.length;
-      });
+        // build files modal for view usages
+        vm.files = {
+          data: files,
+          count: filesCount,
+          distribution: filesDistribution
+        };
 
-      vm.meanRevisions = vm.revisionsCount / parent.dateRange.selected.days;
-      vm.maxPair = _.maxBy(_.toPairs(vm.revisionsByDate), (dateCount) => { return dateCount[1]; });
-      vm.minPair = _.minBy(_.toPairs(vm.revisionsByDate), (dateCount) => { return dateCount[1]; });
-      vm.deviationRevisions = _.reduce(vm.revisionsByDate, (sum, count) => {
-        return sum + Math.pow(count - vm.meanRevisions, 2);
-      }) / parent.dateRange.selected.days;
+        // calculate number of revisions made and distribution of revisions over time
+        const changesCount = changes.length;
+        const changesByDate = _
+          .chain(changes)
+          .groupBy(change => moment(change.date).startOf('day').valueOf())
+          .mapValues(changes => changes.length)
+          .toPairs()
+          .value();
 
-      vm.files = response.data.files;
-      vm.filesCount = vm.files.length;
-      vm.filesByType = _.groupBy(vm.files, 'fileExtension');
+        // calculate mean and deviation of revisions
+        const meanChanges = changesCount / vm.range.days;
+        const maxPair = _.maxBy(changesByDate, (dateCount) => {
+          return dateCount[1];
+        });
+        const minPair = _.minBy(changesByDate, (dateCount) => {
+          return dateCount[1];
+        });
+        const deviationChanges = _.reduce(changesByDate, (sum, pair) => {
+          return sum + Math.pow(pair[1] - meanChanges, 2);
+        }, 0) / vm.range.days;
+
+        // build changes modal for view usages
+        vm.changes = {
+          data: changes,
+          count: changesCount,
+          grouped: changesByDate,
+          mean: meanChanges,
+          max: maxPair ? maxPair[1] : 0,
+          maxDate: maxPair ? moment(parseInt(maxPair[0], 10)).format('DD/MM/YY') : 'N/A',
+          min: minPair ? minPair[1] : 0,
+          minDate: minPair ? moment(parseInt(minPair[0], 10)).format('DD/MM/YY') : 'N/A',
+          deviation: deviationChanges
+        };
+
+        // calculate number of active projects and inactive projects using drive
+        const activeProjects = _.uniqBy(changes, 'projectId');
+        const activeProjectsCount = activeProjects.length;
+        const inactiveProjectsCount = projects.length - activeProjectsCount;
+
+
+        // build projects modal for view usages
+        vm.projects = {
+          active: activeProjects,
+          activeCount: activeProjectsCount,
+          inactiveCount: inactiveProjectsCount
+        };
+
+        // calculate participation count and utilization rate
+        const participatingCount = participating.length;
+        const usersCount = users.length;
+        const utilizationRate = participatingCount / usersCount;
+
+        // build participation and users modal for view usages
+        vm.participation = {
+          data: participating,
+          count: participatingCount,
+          utilization: utilizationRate
+        };
+
+        vm.users = {
+          data: users,
+          count: usersCount
+        };
+      };
+
+      $q
+        .all([
+          Drive.getFiles(vm.range.start, vm.range.end),
+          Drive.getChanges(vm.range.start, vm.range.end),
+          Drive.getParticipatingUsers(vm.range.start, vm.range.end),
+          Users.getUsers(0, vm.range.end),
+          Projects.getProjects(0, vm.range.end)
+        ])
+        .then(processResponse, $log.error);
     };
 
-    Drive
-      .getOverview(parent.dateRange.selected.days)
-      .then(processResponse, $log.error);
-
-    // TODO: To be replaced with dynamic data
-    vm.p_p_5 = [{ data: 15, label: '.xlsx' }, { data: 65, label: '.docx' }, {
-      data: 20,
-      label: '.pdf'
-    }];
-    vm.p_l_3 = [
-      [1, 2],
-      [2, 1.6],
-      [3, 2.4],
-      [4, 2.1],
-      [5, 1.7],
-      [6, 1.5],
-      [7, 1.7]
-    ];
+    (() => {
+      vm.subtitle = 'Collab Statistics on Google Drive Usage';
+      vm.requestData();
+    })();
   }
 })();
