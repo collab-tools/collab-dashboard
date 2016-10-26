0d8b8f8c0b36f589fb3cf2683b98ea9cde607983
Yeap Hooi Tong <hooitongx@gmail.com>
2016-10-26 11:48:30 +0800

    Fixed bug in projects/tasks still using legacy range

--- app/routes/projects/tasks.js
+++ app/routes/projects/tasks.js
@@ -10,13 +10,15 @@ function getTasks(req, res, next) {
   req.query.start = parseInt(req.query.start, 10) || constants.defaults.startDate;
   req.query.end = parseInt(req.query.end, 10) || constants.defaults.endDate;
   req.checkParams('projectId', `projectId ${constants.templates.error.missingParam}`).notEmpty();
-  req.checkQuery('range', `range ${constants.templates.error.missingParam}`).isInt();
+  req.checkQuery('start', `start ${constants.templates.error.invalidData}`).isInt({ min: 0 });
+  req.checkQuery('end', `end ${constants.templates.error.invalidData}`).isInt({ min: 0 });
   const errors = req.validationErrors();
   if (errors) return next(boom.badRequest(errors));
 
   const projectId = req.params.projectId;
   const startDate = moment(req.query.start).format('YYYY-MM-DD HH:mm:ss');
   const endDate = moment(req.query.end).format('YYYY-MM-DD HH:mm:ss');
+
   const response = (tasks) => {
     if (_.isNil(tasks)) return next(boom.badRequest(constants.templates.error.badRequest));
     res.status(200).json(tasks);
